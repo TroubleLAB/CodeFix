59d937c60214bbb90a128c572b0a7f773143647d
Troublesome <112665333+troublesomesync@users.noreply.github.com>
2024-11-10 01:22:52 +0200

    Update CGuild::Invite_Corecrashfix.cpp

--- CGuild::Invite_Corecrashfix.cpp
+++ CGuild::Invite_Corecrashfix.cpp
@@ -5,14 +5,14 @@ Deschidem input_main.cpp și căutam:
 LPCHARACTER newmember = CHARACTER_MANAGER::instance().Find(vid);
 
 /*
-Mai jos găsiți:
+Mai jos găsim:
 */
 
 if (!ch->IsPC())
 	return SubPacketLen;
 
 /* 
-Și o modificați în:    
+Și modificăm în: 
 */
 
 return (!ch->IsPC() || !newmember->IsPC()) ? SubPacketLen : 0; /* @tlb_fix */
